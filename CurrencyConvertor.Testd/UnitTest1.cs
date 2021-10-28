22a1bad764947d2c77404ece7f43221dc3a0e6d8
Faeze-Laptop <fz.dehghan@gmail.com>
2021-10-28 06:48:24 -0700

    removing useless test

--- CurrencyConvertor.Testd/UnitTest1.cs
+++ CurrencyConvertor.Testd/UnitTest1.cs
@@ -7,25 +7,25 @@ using Xunit;
 
 namespace CurrencyConvertor.Testd
 {
-    public class UnitTest1
-    {
-        private readonly Mock<ICurrencyConverter> _currencyConvertor;
+//    public class UnitTest1
+//    {
+//        private readonly Mock<ICurrencyConverter> _currencyConvertor;
 
-        [ExcludeFromCodeCoverage]
-        public UnitTest1(Mock<ICurrencyConverter> currencyConvertor)
-        {
-            _currencyConvertor = currencyConvertor;
-        }
+//        [ExcludeFromCodeCoverage]
+//        public UnitTest1(Mock<ICurrencyConverter> currencyConvertor)
+//        {
+//            _currencyConvertor = currencyConvertor;
+//        }
 
-        [Fact]
-        public void Test1()
-        {
-            _currencyConvertor.Setup(x => x.ClearConfiguration());
-            _currencyConvertor.Setup(x => x.UpdateConfiguration(new List<Tuple<string, string, double>>()));
-            _currencyConvertor.Setup(x => (x.Convert("", "", 1).Result)).Returns(2);
-            //var currencyConvertor = new TaskHiring.Services.CurrencyConverter();
-            var convertorController = new TaskHiring.Controllers.ConverterController(_currencyConvertor.Object);
-            var actionResult = convertorController.Get("", "");
-        }
-    }
+//        [Fact]
+//        public void Test1()
+//        {
+//            _currencyConvertor.Setup(x => x.ClearConfiguration());
+//            _currencyConvertor.Setup(x => x.UpdateConfiguration(new List<Tuple<string, string, double>>()));
+//            _currencyConvertor.Setup(x => (x.Convert("", "", 1).Result)).Returns(2);
+//            //var currencyConvertor = new TaskHiring.Services.CurrencyConverter();
+//            var convertorController = new TaskHiring.Controllers.ConverterController(_currencyConvertor.Object);
+//            var actionResult = convertorController.Get("", "");
+//        }
+//    }
 }
